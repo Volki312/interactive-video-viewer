80965f19e9c6b8824562f106af25bfece6d726f0
Josip Volarević <josip@kupigume.hr>
2020-05-19 11:10:49 +0200

    Track .mp4 files

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-// import video from './video.mp4'
+import video from './video.mp4'
 
 const Home = () => {
 	return (
 		<main>
-			{/* <video className="video" controls autoPlay muted>
+			<video className="video" controls autoPlay muted>
 				<source src={video} type="video/mp4" />
 				Your browser does not support the video tag.
-			</video>  */}
+			</video> 
 		</main>
 	)
 }
